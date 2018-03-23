875c7759a651d76c47080dfdd8176b2192c99c2f
Taylor Dondich <taylor@dondich.com>
2018-03-23 12:00:44 -0700

    Initial commit.

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,52 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <p>"{{message}}"</p>
+    <audio :src="'audio/' + this.audio + '.mp3'" autoplay="true"></audio>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import VueAudio from 'vue-audio';
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    VueAudio
+  },
+  data: () => {
+    return {
+      message: 'Taylor Screws up NES Graphics Programming',
+      audio: '8bit'
+    }
   }
 }
 </script>
 
 <style>
+@font-face {
+font-family: 'textile';
+src: url('fonts/textile.woff2') format('woff2'),
+      url('fonts/textile.woff') format('woff'),
+      url('fonts/textile.ttf') format('truetype');
+font-weight: normal;
+font-style: normal;
+}
+
+
+body, html, #app {
+  height: 100%;
+  width: 100%;
+  background-color: black;
+}
+
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-family: 'textile', Helvetica, Arial, sans-serif;
+  color: white;
+  line-height: 1.5;
+  font-size: 4em;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
